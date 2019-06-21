17ffd1ab8aecb05ea262f3e09fd2eb3c1d0c0624
Shant Tchatalbachian <shant@omniteck.com>
2019-06-21 01:57:11 -0700

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # WIP Adding pre-requisites to installer soon
+/etc/init.d/cron stop
+/etc/init.d/mysqld disable
 mv EvilAPDefender.py ~/
 mv CRONTABS /etc/crontabs/root
 mv EAPD /etc/init.d/eapd
 chmod 744 /etc/init.d/eapd
 chmod +x /etc/init.d/eapd
+/etc/init.d/eapd disable
