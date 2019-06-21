f1d9bfcab24baff014474469cf604e7a4c40accd
Shant Tchatalbachian <shant@omniteck.com>
2019-06-21 01:33:54 -0700

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,1 +1,8 @@
-#WIP
+#!/bin/bash
+# WIP
+Git clone Https://github.com/0mniteck/EvilAPDefender
+mv EvilAPDefender.py ~/
+mv CRONTABS /etc/crontabs/root
+mv EAPD /etc/init.d/eapd
+chmod 744 /etc/init.d/eapd
+chmod +x /etc/init.d/eapd
