009876a4575fef685eed51033aafb21e55c7e87a
Shant Tchatalbachian <shant@omniteck.com>
2019-06-23 20:37:34 -0700

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,6 +5,7 @@ echo "Starting Installer for EAPD at $(date -u)" >> /root/EvilAPDefender.log
 opkg update
 opkg install aircrack-ng
 opkg install mysql-server
+mysql_install_db --force
 opkg install python
 opkg install python-mysqldb
 opkg install python-pip
@@ -24,6 +25,5 @@ chmod +x /etc/init.d/eapdd
 /etc/init.d/eapd disable
 echo "Installer Complete."
 echo "Just run /etc/init.d/eapdd L to start learning mode."
-echo "Then run /etc/init.d/cron enable && /etc/init.d/cron start to begin scheduled runs."
 echo "Installer Complete at $(date -u)" >> /root/EvilAPDefender.log
 exit
