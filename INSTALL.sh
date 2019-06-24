3161023eef85c5aa363f2cbf04295c3a2c24b75f
Shant Tchatalbachian <shant@omniteck.com>
2019-06-23 19:50:13 -0700

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# WIP Adding pre-requisites to installer soon
-# also adding niceness option for nano installs to prevent crashes with extroot
+# WIP adding niceness option for nano installs to prevent crashes with extroot
+echo "Starting Installer for EAPD"
+echo "Starting Installer for EAPD at $(date -u)" >> /root/EvilAPDefender.log
+opkg update
+opkg install aircrack-ng
+opkg install python
+opkg install mysql-server
+opkg install python-mysqldb
+pip install netaddr
+pip install scappy
 /etc/init.d/cron stop
 /etc/init.d/mysqld disable
 mv EvilAPDefender.py ~/
@@ -9,3 +17,5 @@ mv EAPD /etc/init.d/eapd
 chmod 744 /etc/init.d/eapd
 chmod +x /etc/init.d/eapd
 /etc/init.d/eapd disable
+echo "Installer complete"
+echo "Installer complete at $(date -u)" >> /root/EvilAPDefender.log
