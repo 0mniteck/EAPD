09c27e9a6ea953239cdbb932703c8f6055834507
Shant Tchatalbachian <shant@omniteck.com>
2019-06-25 00:51:38 -0700

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -16,9 +16,8 @@ cp -f DAEMON/MYSQLD /etc/init.d/mysqld && cp -f DAEMON/EAPDD /etc/init.d/eapdd
 chmod 744 /etc/init.d/mysqld && chmod 744 /etc/init.d/eapdd
 chmod +x /etc/init.d/mysqld && chmod +x /etc/init.d/eapdd
 /etc/init.d/mysqld disable && /etc/init.d/eapdd disable
-echo "Installer Complete."
+echo "Installer Complete." && echo "Installer Complete at $(date -u)" >> /root/eapd.log
 echo "Log file saved to /root/eapd.log."
 echo "Just run '/etc/init.d/eapdd L' to start learning mode."
-echo "Installer Complete at $(date -u)" >> /root/eapd.log
 echo "|-------------------------------------------END-------------------------------------------|" >> /root/eapd.log
 exit
