93d263bd696c00ee7c9ecd6d534a4dfc366ad27b
Shant Tchatalbachian <shant@omniteck.com>
2019-06-24 17:39:58 -0700

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # WIP adding niceness option for nano installs to prevent crashes with extroot
 echo "Starting Installer for EAPD..."
-echo "Starting Installer for EAPD at $(date -u)" >> /root/EvilAPDefender.log
+echo "Starting Installer for EAPD at $(date -u)" >> /root/eapd.log
 opkg update
 opkg install aircrack-ng
 mkdir /mnt/data/
@@ -13,10 +13,17 @@ pip install netaddr
 pip install scappy
 /etc/init.d/cron stop && /etc/init.d/cron disable
 /etc/init.d/mysqld stop
+mkdir /pineapple/modules/eapd/
+mkdir /pineapple/modules/eapd/api/
+mkdir /pineapple/modules/eapd/js/
 mv -f EAPD.py /root/eapd.py
 mv -f CRONTABS /etc/crontabs/root
-mv -f DAEMON/MYSQLD /etc/init.d/mysqld
-mv -f DAEMON/EAPDD /etc/init.d/eapdd
+mv -f MODULE/MODULE.info /pineapple/modules/eapd/module.info
+mv -f MODULE/MODULE.html /pineapple/modules/eapd/module.html
+mv -f MODULE/MODULE.php /pineapple/modules/eapd/api/module.php
+mv -f MODULE/MODULE.js /pineapple/modules/eapd/js/module.js
+mv -f DAEMON /etc/init.d/mysqld
+mv -f DAEMON /etc/init.d/eapdd
 chmod 744 /etc/init.d/mysqld
 chmod 744 /etc/init.d/eapdd
 chmod +x /etc/init.d/mysqld
@@ -24,7 +31,7 @@ chmod +x /etc/init.d/eapdd
 /etc/init.d/mysqld disable
 /etc/init.d/eapdd disable
 echo "Installer Complete."
-echo "Just run /etc/init.d/eapdd L to start learning mode."
-echo "Installer Complete at $(date -u)" >> /root/EvilAPDefender.log
+echo "Just run /etc/init.d/eapdd L to start learning mode. Log file saved to /root/eapd.log."
+echo "Installer Complete at $(date -u)" >> /root/eapd.log
 echo "|-------------------------------------------END-------------------------------------------|" >> /root/EvilAPDefender.log
 exit
