d0cca0d1d76e76ae2246a577f36144e1484c2375
Shant Tchatalbachian <shant@omniteck.com>
2019-06-25 04:23:56 -0700

    Update UNINSTALL.sh

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -3,10 +3,10 @@ echo "Starting Uninstaller for EAPD..."
 echo "Starting Uninstaller for EAPD at $(date -u)" >> /root/eapd.log
 /etc/init.d/cron stop && /etc/init.d/cron disable && /etc/init.d/eapd stop
 pip uninstall -y netaddr scapy
-opkg --autoremove remove mysql-server python python-mysql python-pip
+opkg --autoremove remove mysql-server mariadb-client python python-mysql python-pip
 rm -f /root/eapd.py && rm -f /etc/crontabs/root
 rm -f /etc/init.d/mysqld && rm -f /etc/init.d/eapdd
-rm -f -r /pineapple/modules/eapd && rm -f -r /mnt/data
+rm -f -r /pineapple/modules/eapd && rm -f -r /mnt/data && rm -f -r /var/run/mysqld/
 echo "Uninstaller Complete." && echo "Uninstaller Complete at $(date -u)" >> /root/eapd.log
 echo "If this is not a Wifi-Pineapple remove Aircrack-ng manually by running 'opkg --autoremove remove aircrack-ng'."
 echo "Log file saved to /root/eapd.log."
