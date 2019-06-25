605368d46b81b65f3daea8441221684b7b3e84f0
Shant Tchatalbachian <shant@omniteck.com>
2019-06-24 21:08:13 -0700

    Create UNINSTALL.sh

---
+++ UNINSTALL.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Starting Uninstaller for EAPD..."
+echo "Starting Uninstaller for EAPD at $(date -u)" >> /root/eapd.log
+/etc/init.d/cron stop && /etc/init.d/cron disable
+/etc/init.d/eapd stop
+opkg remove mysql-server
+pip remove netaddr
+pip remove scappy
+opkg remove python python-mysqldb python-pip
+rm -f /root/eapd.py
+rm -f /etc/crontabs/root
+rm -f -r /pineapple/modules/eapd
+rm -f -r /mnt/data
+rm -f /etc/init.d/mysqld
+rm -f /etc/init.d/eapdd
+echo "Uninstaller Complete."
+echo "Log file saved to /root/eapd.log."
+echo "Just run /etc/init.d/eapdd L to start learning mode."
+echo "Uninstaller Complete at $(date -u)" >> /root/eapd.log
+echo "|-------------------------------------------END-------------------------------------------|" >> /root/eapd.log
+exit
