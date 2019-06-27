c2e7e6b74db3f870b9e94bc4e00a463e8c22bbc8
Shant Tchatalbachian <shant@omniteck.com>
2019-06-27 03:20:19 -0700

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,25 @@
-#!/bin/bash
+#!/bin/ash
+# INSTALL.sh
+
+######################################################################
+#
+# Copyright (C) 2019 Shant Patrick Tchatalbachian
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+######################################################################
+
 echo "Starting Installer for EAPD..." && echo " " && echo "Starting Installer for EAPD at $(date -u)" >> /root/eapd.log
 mkdir /mnt/data/ && mkdir /mnt/data/mysql/ && mkdir /mnt/data/tmp/
 chmod 777 /mnt/data/ && chmod 777 /mnt/data/mysql/ && chmod 777 /mnt/data/tmp/
