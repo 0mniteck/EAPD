9d6b77aac74b9e0404f4f51377a10e312b5213a0
Shant Tchatalbachian <shant@omniteck.com>
2019-07-08 21:19:55 -0700

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -20,7 +20,8 @@
 #
 ######################################################################
 
-echo "Starting Installer for EAPD..." && echo " " && echo "Starting Installer for EAPD at $(date -u)" >> /root/eapd.log
+mkdir /root/logs
+echo "Starting Installer for EAPD..." && echo " " && echo "Starting Installer for EAPD at $(date -u)" >> /root/logs/install.log
 mkdir /mnt/data/ && mkdir /mnt/data/mysql/ && mkdir /mnt/data/tmp/
 chmod 777 /mnt/data/ && chmod 777 /mnt/data/mysql/ && chmod 777 /mnt/data/tmp/
 mkdir /var/log/mysql/ && chmod 777 /var/log/mysql/
@@ -42,8 +43,8 @@ cp -f DAEMON/MYSQLD /etc/init.d/mysqld && cp -f DAEMON/EAPDD /etc/init.d/eapdd
 chmod 744 /etc/init.d/mysqld && chmod 744 /etc/init.d/eapdd
 chmod +x /etc/init.d/mysqld && chmod +x /etc/init.d/eapdd
 /etc/init.d/mysqld disable && /etc/init.d/eapdd disable && mysql_install_db --force && opkg install python-mysql
-echo "Installer Complete." && echo " " && echo "Installer Complete at $(date -u)" >> /root/eapd.log
-echo "Log file saved to /root/eapd.log." && echo " "
+echo "Installer Complete." && echo " " && echo "Installer Complete at $(date -u)" >> /root/logs/install.log
+echo "Log file saved to /root/logs/install.log." && echo " "
 echo "Just run '/etc/init.d/eapdd L' to start learning mode." && echo " "
-echo "|-------------------------------------------END-------------------------------------------|" >> /root/eapd.log
+echo "|-------------------------------------------END-------------------------------------------|" >> /root/logs/install.log
 exit
