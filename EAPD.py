7c277adf4d6e3034cd581d0c26be3fc5755f06d4
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-05 07:10:58 -0800

    Update EAPD.py

--- EAPD.py
+++ EAPD.py
@@ -787,6 +787,7 @@ print "SCANNING FOR WIRELESS NETWORKS"
 print " \n"
 try:
     airodump = Popen(["airodump-ng", "--output-format", "csv",  "-w", "out.csv", mon_iface])
+#5G#airodump = Popen(["airodump-ng", "--output-format", "csv",  "-w", "out.csv", "--channel", "1,2,3,4,5,6,7,8,9,10,11,36,40,44,48,52,56,60,64,100,104,108,112,116,120,124,128,132,136,140,149,153,157,161,165", mon_iface])
     aps = {}
     time.sleep(150)
     airodump.terminate()
