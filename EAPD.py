917c775a567aea1c76a315cc7ce25adbe115783d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-06 05:59:00 -0800

    Update EAPD.py

--- EAPD.py
+++ EAPD.py
@@ -791,7 +791,7 @@ try:
     if frequency == "2":
         airodump = Popen(["airodump-ng", "--output-format", "csv",  "-w", "out.csv", mon_iface])
     elif frequency == "5":
-        airodump = Popen(["airodump-ng", "--output-format", "csv",  "-w", "out.csv", "--channel", "1,2,3,4,5,6,7,8,9,10,11,36,40,44,48,52,56,60,64,100,104,108,112,116,120,124,128,132,136,140,149,153,157,161,165", mon_iface])
+        airodump = Popen(["airodump-ng", "--output-format", "csv",  "-w", "out.csv", "--channel", "36,64,11,132,10,56,8,120,7,6,124,3,5,9,52,1,153,44,140,40,128,4,112,136,161,60,149,104,165,100,116,48,2,108,157", mon_iface])
     else:
         airodump = Popen(["airodump-ng", "--output-format", "csv",  "-w", "out.csv", mon_iface])
     
