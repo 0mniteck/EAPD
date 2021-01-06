d2333a0ea104ccff91a4474945dc7876923ae034
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-06 11:15:20 -0800

    Update UNINSTALL.sh

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -21,7 +21,7 @@
 ######################################################################
 
 rm -f /root/eapd.log && rm -f -r /root/logs
-printf "\nStarting Uninstaller for EAPD...\n\n" && printf "Starting Uninstaller for EAPD at $(date '+%r on %x')\n" >> /root/eapd-uninstall.log
+printf "\033[92m\nStarting Uninstaller for EAPD...\n\n\033[0m" && printf "Starting Uninstaller for EAPD at $(date '+%r on %x')\n" >> /root/eapd-uninstall.log
 /etc/init.d/cron stop && /etc/init.d/cron disable && /etc/init.d/eapdd stop && sleep 5
 python -m pip uninstall -y netaddr scapy wheel
 opkg --autoremove --force-removal-of-dependent-packages remove mariadb-server mariadb-client mariadb-server-extra python python-mysql python-pip
