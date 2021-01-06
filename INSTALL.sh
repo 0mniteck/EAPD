9bbbd7cf5e20a4c2f5b33a61cd138bf64bb57c7d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-06 03:25:28 -0800

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -34,10 +34,10 @@ opkg install mariadb-client
 opkg install mariadb-server-extra
 opkg install python --force-overwrite
 opkg install python-pip --force-overwrite
-#pip install --upgrade pip
-pip install wheel
-pip install netaddr
-pip install scapy
+#python -m pip install --upgrade pip
+python -m pip install wheel
+python -m pip install netaddr
+python -m pip install scapy
 /etc/init.d/cron stop && /etc/init.d/cron disable && /etc/init.d/mysqld stop
 mkdir /pineapple/
 mkdir /pineapple/modules/
@@ -50,15 +50,24 @@ chmod +x /etc/init.d/mysqld && chmod +x /etc/init.d/eapdd
 echo 'innodb_use_native_aio = 0' >> /etc/mysql/conf.d/50-server.cnf
 /etc/init.d/mysqld disable && /etc/init.d/eapdd disable && mysql_install_db --force && opkg install python-mysql
 /etc/init.d/mysqld start && echo " "
-read -s -n 1 -p "Make sure MySQL is fully started!!! then press any key to continue . . . or ctrl+c to stop" && echo " "
-mysql_secure_installation
+read -s -n 1 -p "Make sure MySQL is fully started!!! then press any key to continue . . . or ctrl+c to stop" && echo " " && echo " "
+#mysql_secure_installation
+rootpass=$(openssl rand -base64 16)
+mysql -u root <<-EOF
+UPDATE mysql.user SET Password=PASSWORD('$rootpass') WHERE User='root';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+FLUSH PRIVILEGES;
+EOF
 /etc/init.d/mysqld stop
+echo "Your Mysql password is $rootpass keep it somewhere safe."
 rm /etc/rc.local
 echo '/etc/init.d/eapdd stop' > /etc/rc.local && echo " "
 echo "Installer Complete." && echo " " && echo "Installer Complete at $(date '+%r on %x')" >> /root/logs/install.log
 echo "Log file saved to /root/logs/install.log." && echo " "
 echo "|-----------------------------------------README!-----------------------------------------|" && echo " "
-echo "Add the password you set in MYSQL_SECURE_INSTALLATION to line 666 of /root/eapd.py" && echo " "
+echo "Add the password  in MYSQL_SECURE_INSTALLATION to line 666 of /root/eapd.py" && echo " "
 echo "Then just run '/etc/init.d/eapdd L' to start learning mode." && echo " "
 echo "|-------------------------------------------END-------------------------------------------|" >> /root/logs/install.log
 exit
