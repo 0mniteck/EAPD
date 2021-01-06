76e3374de66579ec7c88063e9eedd541968950f2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-06 03:59:01 -0800

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -21,8 +21,8 @@
 ######################################################################
 
 mkdir /root/logs
-echo "Starting Installer for EAPD..." && echo " " && echo "Starting Installer for EAPD at $(date '+%r on %x')" >> /root/logs/install.log
-read -s -n 1 -p "On models before the MK7, or other openwrt, please look at the wiki under Requirements. Turn off PineAP then Press any key to continue . . . or ctrl+c to stop" && echo " "
+printf "\nStarting Installer for EAPD...\n\n" && printf "Starting Installer for EAPD at $(date '+%r on %x')\n" >> /root/logs/install.log
+read -s -n 1 -p "On models before the MK7, or other openwrt, please look at the wiki under Requirements. Turn off PineAP then Press any key to continue . . . or ctrl+c to stop" && printf "\n\n"
 mkdir /mnt/data/ && mkdir /mnt/data/mysql/ && mkdir /mnt/data/tmp/
 chmod 777 /mnt/data/ && chmod 777 /mnt/data/mysql/ && chmod 777 /mnt/data/tmp/
 mkdir /var/log/mysql/ && chmod 777 /var/log/mysql/
@@ -47,10 +47,10 @@ cp -f EAPD.py /root/eapd.py && cp -f CRONTABS /etc/crontabs/root
 cp -f DAEMON/MYSQLD /etc/init.d/mysqld && cp -f DAEMON/EAPDD /etc/init.d/eapdd
 chmod 744 /etc/init.d/mysqld && chmod 744 /etc/init.d/eapdd
 chmod +x /etc/init.d/mysqld && chmod +x /etc/init.d/eapdd
-echo 'innodb_use_native_aio = 0' >> /etc/mysql/conf.d/50-server.cnf
+printf 'innodb_use_native_aio = 0\n' >> /etc/mysql/conf.d/50-server.cnf
 /etc/init.d/mysqld disable && /etc/init.d/eapdd disable && mysql_install_db --force && opkg install python-mysql
-/etc/init.d/mysqld start && echo " "
-read -s -n 1 -p "Make sure MySQL is fully started!!! then press any key to continue . . . or ctrl+c to stop" && echo " " && echo " "
+/etc/init.d/mysqld start && printf "\n\n"
+read -s -n 1 -p "Make sure MySQL is fully started!!! then press any key to continue . . . or ctrl+c to stop" && printf "\n\n"
 #mysql_secure_installation
 rootpass=$(openssl rand -base64 16)
 mysql -u root <<-EOF
@@ -62,11 +62,11 @@ FLUSH PRIVILEGES;
 EOF
 /etc/init.d/mysqld stop
 rm /etc/rc.local
-echo '/etc/init.d/eapdd stop' > /etc/rc.local && echo " "
-echo "Installer Complete." && echo " " && echo "Installer Complete at $(date '+%r on %x')" >> /root/logs/install.log
-echo "Log file saved to /root/logs/install.log." && echo " "
-echo "|-----------------------------------------README!-----------------------------------------|" && echo " "
-echo "Add the password '$rootpass' to line 666 of /root/eapd.py" && echo " "
-echo "Then just run '/etc/init.d/eapdd L' to start learning mode." && echo " "
-echo "|-------------------------------------------END-------------------------------------------|" >> /root/logs/install.log
+printf '/etc/init.d/eapdd stop\n' > /etc/rc.local && printf "\n\n"
+printf "Installer Complete.\n\n" && printf "Installer Complete at $(date '+%r on %x')\n" >> /root/logs/install.log
+printf "Log file saved to /root/logs/install.log.\n\n"
+printf "|-----------------------------------------README!-----------------------------------------|\n\n"
+printf "Add the password '$rootpass' to line 666 of /root/eapd.py\n\n"
+printf "Then just run '/etc/init.d/eapdd L' to start learning mode.\n\n"
+printf "|-------------------------------------------END-------------------------------------------|\n" >> /root/logs/install.log
 exit
