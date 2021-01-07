d3fb547b00c7f70d49d16bba93917b31ade668d2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-07 07:59:11 -0800

    Update UNINSTALL.sh

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -25,7 +25,7 @@ printf "\033[92m\nStarting Uninstaller for EAPD...\n\n\033[0m" && printf "Starti
 /etc/init.d/cron stop && /etc/init.d/cron disable && /etc/init.d/eapdd stop && sleep 5 && python -m pip uninstall -y netaddr scapy wheel
 opkg --autoremove --force-removal-of-dependent-packages remove mariadb-server mariadb-client mariadb-server-extra python python-mysql python-pip
 opkg --autoremove --force-removal-of-dependent-packages remove mariadb-server mariadb-client mariadb-server-extra python python-mysql python-pip
-rm -f /root/eapd.py && rm -f /etc/crontabs/root && rm -f /etc/init.d/eapdd && rm -f /etc/mysql/conf.d/50-server.cnf
+rm -f /root/eapd.py && rm -f /etc/crontabs/root && rm -f /etc/init.d/eapdd && rm -f /etc/mysql/conf.d/50-server.cnf && rm -f /etc/config/mysqld
 rm -f -r /pineapple/modules/EAPD/ && rm -f -r /mnt/data/ && rm -f -r /var/log/mysql/ && rm -f -r /var/log/mysqld/ && rm -f -r /var/run/mysqld/
 printf "\nUninstaller Complete.\n\n" && printf "Uninstaller Complete at $(date '+%r on %x')\n" >> /root/eapd-uninstall.log
 printf "Log file saved to /root/eapd-uninstall.log.\n\n"
