6a1ae7476017222a2e511b5d2508fd9d8c033da2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-09 08:51:06 -0800

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -26,8 +26,8 @@ printf "\n\n" && opkg update && opkg install mariadb-server --force-overwrite &&
 opkg install python --force-overwrite && opkg install python-pip --force-overwrite
 python -m pip install wheel && python -m pip install netaddr scapy
 /etc/init.d/cron stop && /etc/init.d/cron disable
-rm -f -r /pineapple/modules/EAPD/ && cp -f MODULE/EAPD-master.tar.gz /pineapple/modules/EAPD-master.tar.gz
-tar x -z -f /pineapple/modules/EAPD-master.tar.gz -C /pineapple/modules/ && rm -f /pineapple/modules/EAPD-master.tar.gz
+rm -f -r /pineapple/modules/eapd/ && cp -f MODULE/eapd-master.tar.gz /pineapple/modules/eapd-master.tar.gz
+tar x -z -f /pineapple/modules/eapd-master.tar.gz -C /pineapple/modules/ && rm -f /pineapple/modules/eapd-master.tar.gz
 cp -f EAPD.py /root/eapd.py && cp -f CRONTABS /etc/crontabs/root && cp -f EAPDD /etc/init.d/eapdd
 chmod 500 /etc/init.d/eapdd && chmod 500 /root/eapd.py
 printf "innodb_use_native_aio = 0\n" >> /etc/mysql/conf.d/50-server.cnf
