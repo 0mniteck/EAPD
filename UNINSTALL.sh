cfb361ae189a97cd08c656d385f7d0a08d0b3b53
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-09 10:53:04 -0800

    Update INSTALL.sh

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -21,11 +21,17 @@
 ######################################################################
 
 printf "\033[92m\nStarting Uninstaller for EAPD...\n\n\033[0m" && printf "Starting Uninstaller for EAPD at $(date '+%r on %x')\n" >> /root/eapd-uninstall.log
+read -n 1 -t 25 -p "Keep config files [Y/n]: " config
+if [ $config == "n" ]; then
+  printf "\nClearing configs...\n"
+  rm -f /etc/config/mysqld && rm -f /etc/config/eapdd && rm -f -r /mnt/data/ && rm -f -r /var/log/mysql/ && rm -f -r /var/log/mysqld/ && rm -f -r /var/run/mysqld/
+else
+  printf "\nKeeping configs...\n"
+fi
 /etc/init.d/cron stop && /etc/init.d/cron disable && /etc/init.d/eapdd stop && sleep 5 && python -m pip uninstall -y netaddr scapy wheel
 opkg --autoremove --force-remove --force-removal-of-dependent-packages remove mariadb-server mariadb-client python python-mysql python-pip
 opkg --autoremove --force-remove --force-removal-of-dependent-packages remove mariadb-server mariadb-client python python-mysql python-pip
-rm -f /root/eapd.py && rm -f /etc/crontabs/root && rm -f /etc/init.d/eapdd && rm -f /etc/config/mysqld && rm -f /etc/config/eapdd && rm -f /etc/mysql/conf.d/50-server.cnf
-rm -f -r /mnt/data/ && rm -f -r /var/log/mysql/ && rm -f -r /var/log/mysqld/ && rm -f -r /var/run/mysqld/ && rm -f /root/eapd.log && rm -f -r /root/logs/
+rm -f /root/eapd.py && rm -f /etc/crontabs/root && rm -f /etc/init.d/eapdd && rm -f /etc/mysql/conf.d/50-server.cnf && rm -f /root/eapd.log && rm -f -r /root/logs/
 rm -f /etc/rc.local && printf "exit 0" > /etc/rc.local
 rm -f -r /pineapple/modules/eapd/
 printf "\nUninstaller Complete.\n\n" && printf "Uninstaller Complete at $(date '+%r on %x')\n" >> /root/eapd-uninstall.log
