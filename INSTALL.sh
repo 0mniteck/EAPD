4ae843f1cd51eacb2b2ec45f14c202598d54e382
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2021-01-09 14:57:58 -0800

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -36,7 +36,7 @@ chmod 500 /etc/init.d/eapdd && chmod 500 /root/eapd.py
 printf "innodb_use_native_aio = 0\n" >> /etc/mysql/conf.d/50-server.cnf
 uci set mysqld.general.enabled='1' && uci commit
 rm -f /etc/rc.local && printf "/etc/init.d/eapdd stop\nexit 0" > /etc/rc.local && sleep 10
-/etc/init.d/eapdd disable && mysql_install_db --force && opkg install python-mysql --force-overwrite
+/etc/init.d/eapdd disable && opkg install python-mysql --force-overwrite
 mysql_installation_secure() {
 /etc/init.d/mysqld start && sleep 10 && printf "\nStarting and Securing MYSQL...\n\n"
 if [ -z $rootpass ]; then
@@ -56,6 +56,7 @@ if [ -f /etc/config/eapdd ]; then
   rootpass=$password
   mysql_installation_secure
 else
+  mysql_install_db --force
   mysql_installation_secure
   printf "###################################" > /etc/config/eapdd
   chmod 200 /etc/config/eapdd
@@ -95,6 +96,7 @@ if [ -z $time ]; then
 fi && printf "\n"
 chmod 400 /etc/config/eapdd
 if [ -f /etc/config/eapd-cron ]; then
+  printf ""
 else
   printf "###################################" > /etc/config/eapd-cron
   chmod 200 /etc/config/eapd-cron
